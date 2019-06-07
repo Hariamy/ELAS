e8bc333fc2b072d1e1a100c5cfc2d1576125a766
Hariamy <hariamymv@hotmail.com>
2019-06-07 18:30:40 -0300

    Página de solicitar empréstimo

--- siteELAS/elas/src/paginas/routes.js
+++ siteELAS/elas/src/paginas/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {Router, Route, Redirect, hashHistory} from 'react-router';
 
 import Login from './login/login';
+import Solicitar from './solicitar/solicitar';
 
 
 export default props => (
     <Router history={hashHistory}>
         <Route path='login' component={Login}/>
+        <Route path='solicitar' component={Solicitar}/>
 
         <Redirect from='*' to ='/login'/>
     </Router>
---
+++ siteELAS/elas/src/paginas/solicitar/solicitar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../estilo/App.css';
+
+
+
+
+export default props => {
+	return (
+		<div className="Inicio">
+			<h1>FOI PARA SOLICITAR</h1>
+		</div>
+	);
+}
+
