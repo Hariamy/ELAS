56cf66db19096f86401b4a405159da6954334985
Hariamy <hariamymv@hotmail.com>
2019-06-07 19:02:31 -0300

    Página de solicitar empréstimo

--- siteELAS/elas/src/paginas/solicitar/solicitar.js
+++ siteELAS/elas/src/paginas/solicitar/solicitar.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import '../../estilo/App.css';
 
-
-
+import Barra from './barra'
 
 export default props => {
 	return (
 		<div className="Inicio">
-			<h1>FOI PARA SOLICITAR</h1>
+			<Barra/>
 		</div>
 	);
 }
